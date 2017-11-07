94768a6641bf6d6417431e424bbc771c326c5e31
Jason Nelson <Jasonn318@gmail.com>
2017-11-07 10:32:38 -0600

    reworked solution and fixed bugs

--- strip_string_of_comment.rb
+++ strip_string_of_comment.rb
@@ -1,29 +1,18 @@
-require 'pry'
-
-def solution(string, comment_chars)
-  comment_range = []
-  num_comments = 0
-  i = 0
-
-  string.chars.each do |char|
-    num_comments += 1 if comment_chars.include? char
-  end
-
-  begin_comment = 0
-  num_comments.times do |x|
-    i = 0
-    loop do
-      begin_comment = string =~ /#{comment_chars[i]}/
-      i += 1 if !begin_comment
-      break if begin_comment
+def strip_comments(input, markers)
+  input = input.split("\n")
+  result = []
+  input.each do |sentence|
+    line = ""
+    0.upto(sentence.length-1) do |index|
+      if markers.include? sentence[index]
+        break
+      else
+        line += sentence[index]
+      end
     end
-    end_comment = string =~ /\n/
-    end_comment = string[-1] if !end_comment
-    string.slice!(begin_comment...end_comment)
-    begin_comment = nil
+    result << line.strip
   end
-  puts string
+  result.join("\n")
 end
 
-result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
-# result should == "apples, pears\ngrapes\nbananas"
+p strip_comments "Hello world # !Fuck this" , ['!']

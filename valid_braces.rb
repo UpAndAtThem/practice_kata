994353ad887fc35646b747a595bc21be877e5a8c
Jason Nelson <Jasonn318@gmail.com>
2017-11-13 11:08:06 -0600

    add counting of each substring to determine if valid

--- valid_braces.rb
+++ valid_braces.rb
@@ -4,6 +4,7 @@ INVALID_STARTING = [')', '}', ']']
 require 'pry'
 
 def valid_braces(str)
+  grouping = ''
   paren = 0
   curly = 0
   bracket = 0
@@ -15,34 +16,31 @@ def valid_braces(str)
   loop do
     return false if INVALID_STARTING.any? { |sym| sym == char_arr[0]}
     end_of_group = char_arr.join =~ /#{Regexp.quote(BRACES[char_arr[0]])}/
+    return false if end_of_group == nil
     grouping = char_arr[0..end_of_group]
     char_arr.rotate!(end_of_group + 1)
     count += end_of_group + 1
     break if count == str.length
   end
-  
 
+  grouping.each do |char|
+    paren += 1 if char == '('
+    paren -= 1 if char == ')'
 
-  # braces_arr.each do |char|
-  #   paren += 1 if char == '('
-  #   paren -= 1 if char == ')'
+    curly += 1 if char == '{'
+    curly -= 1 if char == '}'
 
-  #   curly += 1 if char == '{'
-  #   curly -= 1 if char == '}'
+    bracket += 1 if char == '['
+    bracket -= 1 if char == ']'
 
-  #   bracket += 1 if char == '['
-  #   bracket -= 1 if char == ']'
+    return false if [paren, curly, bracket].any? { |total| total < 0}
+  end
 
-  #   return false if [paren, curly, bracket].any? { |total| total < 0}
-  # end
-
-  # return true if [paren, curly, bracket].any? { |total| total == 0}
-
+  return true if [paren, curly, bracket].any? { |total| total == 0}
 end
 
-
 p valid_braces("([{}]){}[]")   #=>  True
-# p valid_braces "([{}])"   #=>  True
-# p valid_braces "(}"       #=>  False
-# p valid_braces "[(])"     #=>  False
-# p valid_braces "[({})](]" #=>  False
+p valid_braces "([{}])"   #=>  True
+p valid_braces "(}"       #=>  False
+p valid_braces "[(])"     #=>  False
+p valid_braces "[({})](]" #=>  False

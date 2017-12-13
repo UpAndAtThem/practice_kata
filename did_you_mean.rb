69700a14efc67996138f1cab7ca45fac472bca3f
Jason Nelson <Jasonn318@gmail.com>
2017-12-13 10:49:49 -0600

    refactored to skip comparison if the word and misspelling are more or less than 2 chars in length apart

--- did_you_mean.rb
+++ did_you_mean.rb
@@ -1,18 +1,21 @@
 class Dictionary
 
   def initialize(words)
-    @words=words
+    @words = words
   end
 
   def closest_word(misspelled_word)
     num_letters_similar_hash = Hash.new(0)
+
     @words.each do |word|
+      next if word.length > misspelled_word.length + 2
+      next if word.length < misspelled_word.length - 2
+
       misspelled_word.split("").each do |x|
         num_letters_similar_hash[word] += 1 if word.include? x
       end
-      num_letters_similar_hash[word] = 0 if word.length > misspelled_word.length + 2
-      num_letters_similar_hash[word] = 0 if word.length < misspelled_word.length - 2
     end
+
     num_letters_similar_hash.max_by{|k,v| v}[0]
   end
 end

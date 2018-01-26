38c6bdaa5da71094c50fb519790414cf0a75c980
Jason Nelson <Jasonn318@gmail.com>
2018-01-25 18:03:14 -0600

    fixed bug in line 23 that deals with all zeros as the ending number.

--- str_increment.rb
+++ str_increment.rb
@@ -11,12 +11,16 @@ def split_leading_zeros(string)
 end
 
 def str_increment(string)
+
   if no_ending_num? string
     return string + '1'
   end
+
   alpha_num_partition_arr = split_end_numbers(string)
+
   if alpha_num_partition_arr[1][0] == '0' 
-    return alpha_num_partition_arr[0] + (alpha_num_partition_arr[1].to_i + 1).to_s if alpha_num_partition_arr[1].chars.all? {|char| char == '0'}  #fixbug in this code.  you need to account for mult leading zeros
+
+    return alpha_num_partition_arr[0] + alpha_num_partition_arr[1][1..-1] + '1' if alpha_num_partition_arr[1].chars.all? {|char| char == '0'} 
     alpha_num_partition_arr[1] = split_leading_zeros(alpha_num_partition_arr[1])
     
     if alpha_num_partition_arr[1][1].chars.all? {|num| num == '9'}
@@ -25,15 +29,11 @@ def str_increment(string)
 
     alpha_num_partition_arr[1][1] = (alpha_num_partition_arr[1][1].to_i + 1).to_s
     return alpha_num_partition_arr.flatten.join
-
   else
+
     alpha_num_partition_arr[1] = (alpha_num_partition_arr[1].to_i + 1).to_s
     alpha_num_partition_arr.join
   end
-
 end
 
-str_increment("foobar00") #== 'foobar1'
-p str_increment("foobar001") == 'foobar002'
-p str_increment('') == '1'
-p str_increment('foobar000') == 'foobar001'
+p str_increment("foobar99999") #== 'foobar2000'

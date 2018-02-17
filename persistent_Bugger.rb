d4b1bf8483f5e8418943f0fcdf4382d335e6ae7e
Jason Nelson <Jasonn318@gmail.com>
2018-02-17 09:34:53 -0600

    add persistent_bugger.rb

---
+++ persistent_Bugger.rb
@@ -0,0 +1,29 @@
+# persistent_bugger
+
+# Write a method, persistence, that takes in a positive parameter num and
+# returns its multiplicative persistence, which is the number of times you must
+# multiply the digits in num until you reach a single digit.
+
+# For example:
+
+#  persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+#                  # and 4 has only one digit
+
+#  persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+#                   # 1*2*6=12, and finally 1*2=2
+
+#  persistence(4) # returns 0, because 4 is already a one-digit number
+
+def persistence(int)
+  current_val = int
+  count = 0
+
+  loop do
+    return count if current_val.to_s.length == 1
+    count += 1
+    int_arr = current_val.to_s.chars.map(&:to_i)
+    current_val = int_arr.inject(:*)
+  end
+end
+
+persistence 999 # => 4

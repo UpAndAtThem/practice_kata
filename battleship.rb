a16d0293b0c939064ca302abd1793de4037e01f0
Jason Nelson <Jasonn318@gmail.com>
2018-02-23 21:42:33 -0600

    add battleship pseudo code

---
+++ battleship.rb
@@ -0,0 +1,11 @@
+# -create 9*9 board
+# -create ships
+# -have player 1 place ships
+#   -each ship should prompt an up down left or right option. and check to see if valid
+#     -whatever direction take ship.length -1 to auto place the ship (so a 4 ship the player doesn't have to choose 4 positions, just the start and direction)
+# -have player 2 place ships
+#     -whatever direction take ship.length -1 to auto place the ship (so a 4 ship the player doesn't have to choose 4 positions, just the start and direction)
+# -take turns attacking until all ships sections of one player are selected
+#   -player 1 attacks with a a2, g8, etc selection a-i and 1-9
+#   -player 2 attacks with a a2, g8, etc selection a-i and 1-9
+# -declare winner

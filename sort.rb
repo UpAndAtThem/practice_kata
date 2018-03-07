f348f7547b6857147b1aafa76f9ebb6d14d8398e
Jason Nelson <Jasonn318@gmail.com>
2018-03-07 16:54:35 -0600

    refactored to ignore case when sorting

--- sort.rb
+++ sort.rb
@@ -1,21 +1,20 @@
-def case_sort(arr)
+def sort(arr)
   sorted = []
   arr_clone = arr.clone
   sorted << arr_clone.pop
+
   loop do
     current_word = arr_clone.pop
-    arr_clone.size
-
     insert_word(sorted, current_word) 
-
     break if arr_clone.size == 0
   end
+
   sorted
 end
 
 def insert_word(sorted, current_word)
   sorted.each_with_index do |sorted_word, index|
-    if current_word < sorted_word
+    if current_word.downcase < sorted_word.downcase
       sorted.insert(index, current_word)
       break
     end
@@ -26,4 +25,4 @@ end
 
 arr = %w(hello world Zerbras space and ZZZ Dogs are cool Xylophone AAA ship aaa yall arabian nights)
 
-p case_sort arr
+p sort arr

51627e6b2a1dc386e839107ef95fa64dc192f07a
Jason Nelson <Jasonn318@gmail.com>
2018-03-28 17:04:59 -0500

    add calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,66 @@
+require 'pry'
+
+OPERATIONS = %w(add subtract divide multiply).freeze
+
+def calculator
+  num1 = get_number.to_i
+  num2 = get_number.to_i
+
+  operation = choose_operation
+  choice operation, num1, num2
+end
+
+def choice(operation, num1, num2)
+  case operation
+  when 'add'
+    add(num1, num2)
+  when 'subtract'
+    subtract(num1, num2)
+  when 'divide'
+    divide(num1, num2)
+  when 'multiply'
+    multiply(num1, num2)
+  end
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def divide(num1, num2)
+  num1 / num2
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def choose_number
+  num = ''
+  loop do
+    prompt 'pick number'
+    num = gets.chomp
+    break if num =~ /[0-9]/
+  end
+  num.to_i
+end
+
+def prompt(str)
+  puts "=> #{str}"
+end
+
+def choose_operation
+  choice = ''
+  prompt 'Choose: add, subtract, multiply, divide'
+  loop do
+    choice = gets.chomp
+    break if OPERATIONS.include?(choice)
+  end
+  choice
+end
+
+calculator

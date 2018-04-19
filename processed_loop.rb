d122fa55b870614de17099ad67a9caf731e99c31
Jason Nelson <Jasonn318@gmail.com>
2018-04-19 11:52:25 -0500

    add processed_loop.rb

--- processed_loop.rb
+++ processed_loop.rb
@@ -2,6 +2,14 @@
 # one time if process_the_loop equals true. Print "The loop wasn't processed!" 
 # if process_the_loop equals false.
 
+
 process_the_loop = [true, false].sample
 
-process_the_loop ? "The loop was processed" : "The loop wasn't processed"
+if process_the_loop
+  loop do
+    puts "The loop was processed"
+    break
+  end
+else
+  puts "The loop wasn't processed"
+end

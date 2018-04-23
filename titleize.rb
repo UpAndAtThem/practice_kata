e221b4db91afe64e27965d9f8ef816fff88e373e
Jason Nelson <Jasonn318@gmail.com>
2018-04-23 14:24:38 -0500

    added a destructive titleize! method version

--- titleize.rb
+++ titleize.rb
@@ -2,4 +2,24 @@ def titleize(str)
   str.split.map {|word| word.capitalize}.join" "
 end
 
+def titleize!(str)
+  counter = 0
+
+  str[0] = str[0].capitalize
+
+  loop do
+    str[counter + 1] = str[counter + 1].capitalize if str[counter] == ' '
+    
+    counter += 1
+    break if counter == str.size
+  end
+  str
+end
+
 book = "the shinning"
+
+titleize book
+puts book
+
+titleize! book
+puts book

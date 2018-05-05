71397dc687841738939d488c190a74c12981e2d7
Jason Nelson <Jasonn318@gmail.com>
2018-05-05 09:25:16 -0500

    refactored code for style convention

--- solve.rb
+++ solve.rb
@@ -1,10 +1,10 @@
 def solve(string)
   while string =~ /\(/
     string.gsub!(/(\d*)\((\w*)\)/) do |match| 
-    ($2 * $1.to_i) 
+      $2 * $1.to_i
     end
   end
   string
 end
 
-p solve("2(a3(b))")
+solve("2(a3(1(Z)))")

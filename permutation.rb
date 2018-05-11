0889331edde185191aecb08d978cd38390102deb
Jason Nelson <Jasonn318@gmail.com>
2018-05-11 01:41:17 -0500

    add permutation.rb

---
+++ permutation.rb
@@ -0,0 +1,11 @@
+def wildcard(string)
+  result = []
+  
+  combinations = [0,1].repeated_permutation(string.split("").count('?')).to_a
+  combinations.size.times do |index|
+    result << string.split("").map { |char| char == '?' ? combinations[index].shift : char }.join("")
+  end
+  result
+end
+
+wildcard '1?1?' # -> ['1010', '1110', '1011', '1111']

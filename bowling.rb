534b185560badb5c9df64c142b1ec8ef8acb4e1b
Jason Nelson <Jasonn318@gmail.com>
2018-05-13 10:27:57 -0500

    refactored to make game_details hash to be used throughout the program

--- bowling.rb
+++ bowling.rb
@@ -7,33 +7,51 @@ def bowl
   pins_hit = 0
   frame_count = 0
 
+  game_details = {score_card: score_card, frame: 1, pins: 10,
+                  pins_hit: 0, frame_count: 0}
+
   loop do
     2.times do |_|
 
-      pins = 10 if score_card[frame -1].empty?
+      game_details[:pins] = 10 if game_details[:score_card][game_details[:frame] -1].empty?
 
-      pins_hit = roll(pins)
-      pins -= pins_hit
+      game_details[:pins_hit] = roll(game_details[:pins])
+      game_details[:pins] -= game_details[:pins_hit]
 
-      score_card[frame - 1] << pins_hit   
+      game_details[:score_card][game_details[:frame] - 1] << game_details[:pins_hit] 
     end
 
-    frame_count = score_card[frame-1].reduce(&:+)
+    game_details[:frame_count] = game_details[:score_card][game_details[:frame]-1].reduce(&:+)
 
-    if score_card[frame-1][0] == 10
-      score_card[frame-1] = ['X']
-    elsif frame_count == 10 && score_card[frame-1].size == 2
-      score_card[frame-1][1] = "/"
+    if game_details[:score_card][game_details[:frame]-1][0] == 10
+     game_details[:score_card][game_details[:frame]-1] = ['X']
+    elsif game_details[:frame_count] == 10 && game_details[:score_card][game_details[:frame]-1].size == 2
+      game_details[:score_card][game_details[:frame]-1][1] = "/"
     end
 
-    frame += 1
-    break if frame > 10
+    game_details[:frame] += 1
+    break if game_details[:frame] > 9
   end
-  score_card
+  tenth_frame(game_details)
+  
+  game_details
+
+end
+
+def ai_difficulty(mode)
+
+end
+
+def tenth_frame(game_details)
+  game_details
 end
 
 def roll(pins)
-  (0..(pins)).to_a.sample
+  (0..(pins)).to_a.sample()
+end
+
+def add_frame(score_card, score, frame)
+
 end
 
 p bowl

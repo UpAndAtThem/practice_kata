d44ccbcfc44e7e5640aa364d71241e2481e4274b
Jason Nelson <Jasonn318@gmail.com>
2018-05-15 23:01:32 -0500

    refactored out the game_hash

--- bowling.rb
+++ bowling.rb
@@ -2,40 +2,35 @@ require 'pry'
 
 def bowl
   score_card = [[],[],[],[],[],[],[],[],[],[]]
+
   frame = 1
   pins = 10
   pins_hit = 0
   frame_count = 0
 
-  game_details = {score_card: score_card, frame: 1, pins: 10,
-                  pins_hit: 0, frame_count: 0}
-
   loop do
     2.times do |_|
 
-      game_details[:pins] = 10 if game_details[:score_card][game_details[:frame] -1].empty?
+      pins = 10 if score_card[frame -1].empty?
 
-      game_details[:pins_hit] = roll(game_details[:pins])
-      game_details[:pins] -= game_details[:pins_hit]
+      pins_hit = roll(pins)
+      pins -= pins_hit
 
-      game_details[:score_card][game_details[:frame] - 1] << game_details[:pins_hit] 
+      score_card[frame - 1] << pins_hit 
     end
 
-    game_details[:frame_count] = game_details[:score_card][game_details[:frame]-1].reduce(&:+)
+    frame_count = score_card[frame-1].reduce(&:+)
 
-    if game_details[:score_card][game_details[:frame]-1][0] == 10
-     game_details[:score_card][game_details[:frame]-1] = ['X']
-    elsif game_details[:frame_count] == 10 && game_details[:score_card][game_details[:frame]-1].size == 2
-      game_details[:score_card][game_details[:frame]-1][1] = "/"
+    if score_card[frame-1][0] == 10
+     score_card[frame-1] = ['X']
+    elsif frame_count == 10 && score_card[frame-1].size == 2
+      score_card[frame-1][1] = "/"
     end
-
-    game_details[:frame] += 1
-    break if game_details[:frame] > 9
+    #binding.pry
+    frame += 1
+    break if frame > 9
   end
-  tenth_frame(game_details)
-  
-  game_details
-
+  score_card
 end
 
 def ai_difficulty(mode)
@@ -50,13 +45,19 @@ def roll(pins)
   (0..(pins)).to_a.sample()
 end
 
-def add_frame(score_card, score, frame)
+def add_frame(game_details)
+  last_three_games = (score_card[frame-3..frame-1])
+  if frame < 10
+    if score_card[frame- 2].include?('/') 
+      score += score
+    elsif false 
 
+    end
+  elsif false
+    
+
+  end
+  
 end
 
 p bowl
-
-# roll 10
-# roll 10
-# roll 10
-# 

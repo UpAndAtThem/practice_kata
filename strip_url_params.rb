e7f6ce01718fd525cd9fd0527c6e4641501c2003
Jason Nelson <Jasonn318@gmail.com>
2018-05-22 16:11:27 -0500

    add strip_url_params.rb

---
+++ strip_url_params.rb
@@ -0,0 +1,28 @@
+def strip_url_params(url, to_delete = [])
+  domain, query = url.split('?')
+
+  return url unless query
+
+  non_duplicate = remove_duplicate_params(query)
+
+  to_delete.each { |param| non_duplicate.delete(param) } unless to_delete.empty?
+
+  result = domain + '?' + non_duplicate.values.join('&')
+
+  return result[0..-2] if result[-1] == '?'
+  result
+end
+
+def remove_duplicate_params(query)
+  non_duplicate = {}
+
+  query.scan(/(([\w])=\w+)/)
+       .each_with_object([]) do |match, keys_arr|
+         non_duplicate[match[1]] = match[0] unless keys_arr.include?(match[1])
+         keys_arr << match[1]
+       end
+
+  non_duplicate
+end
+
+p strip_url_params("www.codeschool.com?a=1&b=hello&c=2&b=2", ['b'])

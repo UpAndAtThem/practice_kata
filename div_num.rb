cc31227d400d0490d7b45d0ddb6f5c21a7509058
Jason Nelson <Jasonn318@gmail.com>
2018-05-29 13:28:54 -0500

    refactored both methods with with_object method to get rid of explicit collection data type initialization

--- div_num.rb
+++ div_num.rb
@@ -13,17 +13,13 @@
 # return result_arr.length
 
 def div_num(start_range, end_range)
-  result_hsh = {}
-  start_range.upto(end_range) do |current_num|
-    result_hsh[current_num] = get_divisors current_num
-  end
-  result_hsh.max_by { |_, v| v.length }[0]
+  start_range.upto(end_range)
+             .with_object({}) { |current_num, result_hsh| result_hsh[current_num] = get_divisors current_num }
+             .max_by { |_, v| v.length }.first
 end
 
 def get_divisors(number)
-  result_arr = []
-  1.upto(number) { |divisor| result_arr << divisor if number % divisor.zero? }
-  result_arr
+  1.upto(number).with_object([]) { |divisor, result_arr| result_arr << divisor if (number % divisor).zero? }
 end
 
 p div_num(15, 30) # => 24

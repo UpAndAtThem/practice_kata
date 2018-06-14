9990154a771270fcf45d76ea951fab1f543c5917
Jason Nelson <Jasonn318@gmail.com>
2018-06-13 19:29:14 -0500

    added divided by and times operations and changed regex in evaluate_first_operation

--- computer.rb
+++ computer.rb
@@ -1,9 +1,7 @@
-require 'pry'
-
 NUMBERS_HSH = %w(zero one two three four five six seven eight nine ten).zip((0..10)).to_h
 
 def computer(expression)
-  expression = convert_expression(expression).join" "
+  expression = convert_expression(expression).join" " 
 
   loop do
     evaluate_first_operation(expression)
@@ -14,14 +12,17 @@ def computer(expression)
 end
 
 def evaluate_first_operation(expression)
-  expression.sub!(/(-?\w+) (\w+) (-?\w+)/) do |match|
+  expression.sub!(/(-?\w+) (divided by|\w+) (-?\w+)/) do |match|
+
     num1 = $1.to_i
     num2 = $3.to_i
     operator = $2
 
     case operator
-    when 'plus'   then num1 + num2
-    when 'minus'  then num1 - num2
+    when 'plus'       then num1 + num2
+    when 'minus'      then num1 - num2
+    when 'divided by' then num1 / num2
+    when 'times'      then num1 * num2
     end
   end
 end
@@ -32,4 +33,4 @@ def convert_expression(expression)
   end
 end
 
-p computer "three minus three minus three minus three plus ten plus ten" #=> "14"
+computer "ten times ten times ten times two plus one" #=> "2001"

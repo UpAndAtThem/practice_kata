c692369c23bcc593bfbea47e97975eecd1df3d02
Jason Nelson <Jasonn318@gmail.com>
2018-07-16 00:18:22 -0500

    finished sudoku rules method.rb

--- sudoku.rb
+++ sudoku.rb
@@ -1,23 +1,33 @@
 def validSolution(board)
-  board.all? { |row| row.sort == (1..9).to_a}
+  return false unless board.all? { |row| row.sort == (1..9).to_a}
+  return false unless board.transpose.all? { |row| row.sort == (1..9).to_a}
+
   board_copy = board.map(&:dup)
   square = []
+  count = 0
+  top_row = 0
+  middle_row = 1
+  bottom_row = 2
 
-  3.times do |index|
-    square << board_copy[index].shift
-    square << board_copy[index].shift
-    square << board_copy[index].shift
+  loop do 
+    3.times do |index| 
+     square << board_copy[top_row].shift
+     square << board_copy[middle_row].shift
+     square << board_copy[bottom_row].shift
+    end
+
+    return false unless square.sort == (1..9).to_a
+
+    square = []
+    count += 1
+
+    if count % 3 == 0
+      top_row += 3
+      middle_row += 3
+      bottom_row += 3
+    end
+
+    break if count == 9
   end
+  true
 end
-
-validSolution [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]

c6c57747080b59b334aeba44c9c26ea57313307f
Jason Nelson <Jasonn318@gmail.com>
2018-08-09 17:59:47 -0500

    variable_scope.rb

--- variable_scope.rb
+++ variable_scope.rb
@@ -1,9 +1,14 @@
 class Person
   @@pi = "blueberry"
+  PI = "blueberry"
 
   def initialize(name, age)
     @name = name
     @age = age
+  end
+
+  def which_pi
+    puts "Some prefer #{PI} pie, #{@name} prefers #{self.class::PI}"
   end
 end
 
@@ -16,13 +21,22 @@ module Grading
 end
 
 class Teacher < Person
-  @@pi = 3.14 ## the class variable initialized as "blueberry" in Person was just overridden.  There is only 1 copy for all inherited classes/modules
-  
+  @@pi = 3.14 # The class variable initialized as "blueberry" in Person was just overridden.  There is only 1 copy for all inherited classes/modules
+  PI = 3.14
+
   include Grading
 
   def initialize(name, age, subject)
     super(name, age)
     @subject = subject
+  end
+
+  def constant_pi # we would invoke the following 2 methods inorder to access them in Grading module
+    PI
+  end
+
+  def class_var_pi
+    @@pi
   end
 end
 
@@ -40,5 +54,7 @@ cletus = Person.new("Cluetus", 22)
 
 homework = [HomeWork.new("Billy", "Trials of Salem"), HomeWork.new("Jennifer", "Hanson: 20 years later")] 
 
-cindy.grade(homework) #mutates the collection of Homework objects
+cindy.grade(homework) # Mutates the collection of HomeWork objects
 
+cindy.which_pi
+cletus.which_pi

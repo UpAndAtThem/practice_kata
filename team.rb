593900b40c54b78d73033900c04f31e06a1baeeb
Jason Nelson <Jasonn318@gmail.com>
2018-08-22 18:58:27 -0500

    add team.rb

--- team.rb
+++ team.rb
@@ -1,31 +1,59 @@
 class Player
-  attr_accessor :name, :number
+  attr_accessor :name, :age, :stats
 
-  def initialize(name, number)
-    self.name = name
-    self.number = number
+  def initialize(name, age, stats = [])
+    @name = name
+    @age = age
+    @stats = stats
+  end
+
+  def to_s
+    "#{name}"
   end
 end
 
 class Team
-  attr_accessor :name, :members
+  attr_accessor :roster, :name
 
-  def initialize(name, members)
+  def initialize(roster, name)
+    self.roster = roster
     self.name = name
-    self.members = members
   end
 
-  def[](index)
-    members[index]
+  def [](index)
+    roster[index]
   end
 
-  def[]=(index, new_player)
-    members[index] = new_player
+  def +(new_teammate)
+    begin
+      raise(TypeError, "Wrong type") unless (new_teammate.class == Player)
+
+      self.roster += [new_teammate]
+    rescue TypeError => e
+      p "Wrong argument type. #{new_teammate} was not added to team"
+      return nil
+    end
+
+    new_teammate
+  end
+
+  def to_s
+    roster[0..-2].map(&:name).join(", ") + ", and #{roster[-1].name}"
   end
 end
 
-michael_jordan = Player.new("Michael Jordan", "23")
-scotty_pippin = Player.new("Scotty Pippen", "33")
-horace_grant = Player.new("Horace Grant", "54")
+bulls_roster = [Player.new("Michael Jordan", 18, ["GOAT"]), 
+                Player.new("Scottie Pippen", 18),
+                Player.new("Steve Kerr", 19),
+                Player.new("Horace Grant", 20)]
 
-dream_team = Team.new("Bulls", [michael_jordan, scotty_pippin, horace_grant])
+celtics_roster = [Player.new("Larry Bird", 19),
+                  Player.new("Kevin Mchale", 18),
+                  Player.new("Robery Parish", 20),
+                  Player.new("Bill Walton", 21)]
+
+bulls = Team.new(bulls_roster, "Chicago Bulls") 
+celtics = Team.new(celtics_roster, "Boston Celtics")
+
+
+p bulls[0]

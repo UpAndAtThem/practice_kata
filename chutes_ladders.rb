fb3b176e474836995fb47d65bfadddb35e01bac6
Jason Nelson <Jasonn318@gmail.com>
2018-09-20 23:07:32 -0500

    add chutes_ladders.rb

---
+++ chutes_ladders.rb
@@ -0,0 +1,20 @@
+# 1-4 players of chutes and ladders spin the spinner to determine how many squares to go. 
+# players start at square # 1 and move the number of squares the spinner displays
+# if they end up at the bottom of a ladder they go up.
+# if they end up at the top of a chute they go down
+# players take turns until one player reaches square # 100
+
+# Nouns
+#   players
+#   chute
+#   ladder
+#   spinner
+#   square
+
+# Verbs
+#   spin
+#   move
+#   displays
+#   go down
+#   go up
+

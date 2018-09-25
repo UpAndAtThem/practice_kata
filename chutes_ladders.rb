60b4402de60067faf17cf6673aa4d4cbc728bf09
Jason Nelson <Jasonn318@gmail.com>
2018-09-25 18:18:51 -0500

    added more messages in player_spin

--- chutes_ladders.rb
+++ chutes_ladders.rb
@@ -1,40 +1,7 @@
-# 1-4 players of chutes and ladders spin the spinner to determine how many squares to go. 
-# players start at square # 1 and move the number of squares the spinner displays
-# if they end up at the bottom of a ladder they go up.
-# if they end up at the top of a chute they go down
-# players take turns until one player reaches square # 100
-
-# Nouns
-#   players
-#   chute
-#   ladder
-#   spinner
-#   square
-
-# Verbs
-#   spin
-#   move
-#   displays
-#   go down
-#   go up
-
-# set number of players
-# set player names
-  # players move
-  #  loop over each player
-  #    player spins
-  #    player moves
-  #    check for winner
-  #  loop over players until winner when player lands on square 100 exactly.
-
-# Roll
-# move to square
-# check square status
-# add player to square
-
 require 'pry'
 require 'yaml'
 
+# Displayable Module
 module Displayable
   def display_line_positions
     system 'clear'
@@ -42,35 +9,38 @@ module Displayable
       if players[player_index].position.zero?
         puts players[player_index].name[0]
       else
-        puts "#{" " * ((players[player_index].position))}#{players[player_index].name[0]}"
+        puts "#{' ' * players[player_index].position}" \
+               "#{players[player_index].name[0]}"
       end
     end
-    puts "#{" " + "-" * 100}"
+    puts ' ' + ('-' * 100).to_s
     gets
   end
 
   def show_next_six(player)
     possible_landings = board.next_six player
-
   end
 
+  # rubocop:disable MethodLength
   def bottom_latter(player)
-    [ " ___________________________",
-     "| \\               \\         |",
-     "|  \\---------------\\        |",
-     "|   \\               \\       |",
-     "|    \\               \\      |",
-     "|     \\---------------\\     |",
-     "|      \\               \\    |",
-     "|       \\               \\   |",
-     "|        \\---------------\\  |",
-     "|         \\               \\ |",
-     "|          \\               \\|",
+    [' ___________________________',
+     '| \\               \\         |',
+     '|  \\---------------\\        |',
+     '|   \\               \\       |',
+     '|    \\               \\      |',
+     '|     \\---------------\\     |',
+     '|      \\               \\    |',
+     '|       \\               \\   |',
+     '|        \\---------------\\  |',
+     '|         \\               \\ |',
+     '|          \\               \\|',
      "|            #{player.position}              |",
-     "|___________________________"]
-   end
+     '|___________________________']
+  end
+  # rubocop:enable MethodLength
 end
 
+# Player class
 class Player
   attr_accessor :name, :spin, :position
 
@@ -82,12 +52,9 @@ class Player
   def rolled_position
     position + spin
   end
-
-  def move
-
-  end
 end
 
+# ChutesLaddersGame class
 class ChutesLaddersGame
   include Displayable
   attr_reader :players, :spinner, :board
@@ -100,21 +67,18 @@ class ChutesLaddersGame
 
   def set_num_players
     loop do
-      puts "How many people are playing?"
+      puts 'How many people are playing?'
       num_players = gets.chomp.to_i
 
-      if (1..6).include? num_players
-        return @num_players = num_players
-      end
+      return @num_players = num_players if (1..6).cover? num_players
     end
   end
 
   def set_players
     set_num_players
-    count = 1
 
-    @players = (1..@num_players).each.with_object([]) do |count, players_arr| 
-      players_arr << Player.new("Player#{count}")
+    @players = (1..@num_players).each.with_object([]) do |num, players_arr|
+      players_arr << Player.new("Player#{num}")
     end
   end
 
@@ -122,44 +86,44 @@ class ChutesLaddersGame
     !name.strip.empty?
   end
 
-  def set_name(player)
+  def add_name(player)
     loop do
       print "#{player.name} what is your name?: "
       name = gets.chomp
 
       if valid_name? name
-        player.name = name 
+        player.name = name
         break
       end
-      puts "That is an invalid name"
+      puts 'That is an invalid name'
     end
   end
 
   def set_players_name
     players.each do |player|
-      set_name player
+      add_name player
     end
   end
 
   def player_spin(player)
-    puts "Press enter to spin"
+    puts "#{player.name}, press enter to spin"
     gets
     player.spin = spinner.spin
-    puts "You rolled a #{player.spin}"
-    puts "Press enter to advance"
+    puts "#{player.name}, you rolled a #{player.spin}"
+    puts 'Press enter to advance'
     gets
   end
 
   def winner?
-    players.any? { |player| player.position == 100}
+    players.any? { |player| player.position == 100 }
   end
 
   def update_player_position(player)
     player.position = if board[player.rolled_position].jump_to
-      board[player.rolled_position].jump_to
-    else
-      player.rolled_position
-    end
+                        board[player.rolled_position].jump_to
+                      else
+                        player.rolled_position
+                      end
   end
 
   def game_loop
@@ -181,7 +145,7 @@ class ChutesLaddersGame
 
         if winner?
           display_line_positions
-          return
+          return nil
         end
       end
     end
@@ -191,10 +155,11 @@ class ChutesLaddersGame
     set_players_name
     game_loop
     display_line_positions
-    puts "Game Over!"
+    puts 'Game Over!'
   end
 end
 
+# Spinner class
 class Spinner
   def initialize
     @position = 0
@@ -205,9 +170,14 @@ class Spinner
   end
 end
 
+# Square class
 class Square
-  LADDERS = {4 => 14, 9 => 31, 21 => 42, 28 => 84, 36 => 44, 51 => 67, 71 => 91, 80 => 100}
-  CHUTES = {98 => 78, 95 => 75, 93 => 73, 87 => 24, 62 => 19, 64 => 60, 58 => 53, 48 => 26, 49 => 11, 16 => 6}
+  LADDERS = { 4 => 14, 9 => 31, 21 => 42, 28 => 84,
+              36 => 44, 51 => 67, 71 => 91, 80 => 100 }.freeze
+
+  CHUTES = { 98 => 78, 95 => 75, 93 => 73, 87 => 24,
+             62 => 19, 64 => 60, 58 => 53, 48 => 26,
+             49 => 11, 16 => 6 }.freeze
 
   attr_reader :players, :number, :type, :jump_to
 
@@ -242,19 +212,21 @@ class Square
     end
   end
 
+  # rubocop:disable MethodLength
   def set_type
     if top_ladder?
-      "top ladder"
+      'top ladder'
     elsif bottom_ladder?
-      "bottom ladder"
+      'bottom ladder'
     elsif top_chute?
-      "top chute"
+      'top chute'
     elsif bottom_chute?
-      "bottom chute"
+      'bottom chute'
     else
-      "normal"
+      'normal'
     end
   end
+  # rubocop:enable MethodLength
 
   def remove(player)
     players.delete player
@@ -265,53 +237,52 @@ class Square
   end
 end
 
+# Board class
 class Board
-    attr_reader :squares
-    
-    def initialize(players)
-      @squares = (0..100).map do |number|
-        if number == 0
-          Square.new number, players
-        else
-          Square.new number
-        end
-      end
-    end
+  attr_reader :squares
 
-    def remove_from_current_square(player)
-      self[player.position].remove player
-    end
-
-    def add_to_square(square_position, player)
-      self[square_position].players << player
-    end
-
-    def add_player_to_square(player)
-      landed_on_square = self[player.rolled_position]
-      warp_position = landed_on_square.jump_to
-
-      if landed_on_square.bottom_ladder? || landed_on_square.top_chute?
-        add_to_square(warp_position, player)
+  def initialize(players)
+    @squares = (0..100).map do |number|
+      if number.zero?
+        Square.new number, players
       else
-        add_to_square(player.rolled_position, player)
+        Square.new number
       end
     end
+  end
 
-    def advance(player)
-      remove_from_current_square(player)
-      self.add_player_to_square(player)
-    end
+  def remove_from_current_square(player)
+    self[player.position].remove player
+  end
 
-    def [](position)
-      squares[position]
-    end
+  def add_to_square(square_position, player)
+    self[square_position].players << player
+  end
 
-    def next_six(player)
-      squares.slice(player.position, (player.position + 6))
+  def add_player_to_square(player)
+    landed_on_square = self[player.rolled_position]
+    warp_position = landed_on_square.jump_to
+    if landed_on_square.bottom_ladder? || landed_on_square.top_chute?
+      add_to_square(warp_position, player)
+    else
+      add_to_square(player.rolled_position, player)
     end
+  end
+
+  def advance(player)
+    remove_from_current_square(player)
+    add_player_to_square(player)
+  end
+
+  def [](position)
+    squares[position]
+  end
+
+  def next_six(player)
+    squares.slice(player.position, (player.position + 6))
+  end
 end
 
 game = ChutesLaddersGame.new
 
 game.play
-

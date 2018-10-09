81ea8d0e916470e3f575861007bee267f278a112
Jason Nelson <Jasonn318@gmail.com>
2018-10-08 20:22:48 -0500

    added more to assessment notes

--- prac.rb
+++ prac.rb
@@ -1,21 +1,22 @@
-# Object Oriented programming is useful because it creates an encapsulated object that has its own scope where its code runs uninpeded 
-# from the outside world.  It creates a polymorphic interface which helps when you want the same interface with a different outcome.  
+# OOP BENEFITS
+
+# Object Oriented programming is useful because it creates encapsulated objects that have their own scope where the code runs uninpeded 
+# from the rest of the program. It creates a polymorphic interface which helps when you want the same interface for differing objects with a different outcome.  
 # Its easier to scale because instead of one big piece of code running, you have standalone objects that can be expanded and refactored 
 # without causing a huge butterfly effect in the codebase. Plus object orient programming allows programmers to think about
-# code in an abstract ways with nouns acting as classes and verbs class behavior.
+# code in an abstract way, with nouns acting as classes, and verbs being the classes behavior.
 
 # ---------------------------------------------------
 
 # ENCAPSULATION
 
-# hiding functionality and state away from outside objects and code from having access to the encapsulated code. Effectively keeping the code safe 
-# from outside interference, and only exposing publically the interfaces (instance and class methods) 
+# Hiding functionality and state from outside code. Effectively keeping the code safe from outside interference, by controling the access to the encapsulated code, only exposing publically the interfaces
 # that which are needed to properly interact with the object itself. You can set public, private, and protected methods for a class, It will help you control access levels,
 # which will affect the interface of how instantiated objects can interact with its internals.
 
 # instantiated objects do not have access to protected methods outside the class definition, but is treated like a public method from within the class definition.
-# instantiated objects never have access to private methods outside or inside the class.  Private methods are only accessible from other methods in the class. private methods cannot be invoked with an explicit calling object (# EX self.private_method or Mcgruff.private_method)
-# public methods are available anywhere, no access control is enforced. public methods are the interfaces objects use externally, all methods are public by default unless it follows the keyword private or protected.
+# instantiated objects never have access to private methods outside or inside the class.  Private methods are only accessible from other methods in the class. private methods cannot be invoked with an explicit calling object (# EX self.private_method or mcgruff.private_method)
+# public methods are available anywhere, no access control is enforced. Public methods are the interfaces objects use externally, all methods are public by default unless it follows the keyword private or protected.
 
 class Person
   def initialize(name, age)
@@ -45,8 +46,8 @@ rick.greeting # can access name and age i-methods because it's being invoked wit
 
 # INHERITANCE
 
-# Allows behaviors to be passed down from a superclass to all of its subclasses.  A way to achieve polymorphism 
-# as well as resusing code that shares common behavior among the inheritance structure without the need for repetion of code.  Behavior from superclasses 
+# is behaviors inherited from a superclass to all of its subclasses.  A way to achieve polymorphism as well as resusing code that shares common behavior among the inheritance structure.  
+# Extract shared behavior from multiple classes into a superclass, which in turn you forego the need for repetion of code.  Behavior from superclasses 
 # are inherited by all subclasses, but not the other way around. Superclasses inherit nothing from its subclasses. In order to subclass the symbol < is used after the class name in the definition followed 
 # by the superclass that particular class is subclassing.
 
@@ -88,25 +89,30 @@ Cat.new.swim
 # use inheritance if its an "is a" relationship. Use modules if its a "has an ability" relationship.  For instance choose 
 # inheritance for hierachical relationships. ex: Beagle inherits from Mammal which inherits from Animal.
 # use modules for behaviors or abilities. ex: the ability to fly would use the flyable module.
-# modules are a collection of behaviors that can be mixed into multiple classes, unlike inheritance class can mixin as many modules they need, 
+# modules are a collection of behaviors that can be mixed into multiple classes, unlike inheritance, classes can mixin as many modules they need, 
 # where as a class can only subclass from one class.  modules acts similarly to inheritance, but modules cannot be instantiated like a class can.
 
-# The classes Fish and Mammal both subclass from Animal because it has an "is a" relationship, where the module Swimmable is mixed in to the Dog and Fish classes,
-# because this is a "has an ability" relationship.  
+# In the above code, the classes Fish and Mammal both subclass from Animal, again, because it has an "is a" relationship (A fish and a mammal are animals). We also want to reuse the swim functionality without having to 
+# put the same code into 2, 3, 4 or however many classes we need to grant the ability to swim. swim wont fit into Mammal or Animal classes because not all of their subclasses share that behavior. In order to solve this we need to use a module.
+# The module Swimmable is created, and mixed in to the Dog and Fish classes with the keyword include followed by the module name, this will now give those classes that shared behavior.
+  
 
 # METHOD LOOKUP PATH
 
-# method look up path is the path ruby takes when trying to do method resolution.  Ruby first looks within the class the calling object belongs to.  
-# Ruby will then look through all of the mixed in modules in reverse order from which they were included. Ruby will then use inheritance checking 
+# method look up path is the path ruby takes when trying to do method resolution.  Ruby first looks within the class that the calling object belongs to.  
+# Ruby will then look through all of the mixed in modules in reverse order from which they were included. Ruby will then use inheritance, checking 
 # the superclass of the calling object, followed by the superclasses modules, again in reverse order of inclusion.  
-# Ruby will Check The superclasses superclass and then modules until it reaches the BasicObject class, then looks through its methods and modules.  
+# Ruby will Check The superclasses superclass, then the modules, until it reaches the BasicObject class. It then looks through BasicObject's methods and modules.  
 # If at this point there is no resolution, ruby was unable to find the method it was searching for, and will throw a NoMethodError exception and crash the program.
 
-# To check for the method lookup path of a class you can use the class method 'ancestors' 
+# To check for the method lookup path of a class you can use the class method 'ancestors'
+
+# In the above code the Method lookup path of a Fish object would be Fish, Swimmable, Animal, Object, Kernel, BasicObject
 
 # CLASS
 
-# Classes act as a blueprint or a basic outline for objects.  A class defines, and encapsulates an objects state and behavior. State tracks attributes, and is created at the object level
+# Classes act as a blueprint or a basic outline for objects. Classes are encapsulated code with pulbic interfaces that acts as an abstraction of real world objects (IE: Person class, Box class, Dice class, etc)
+# A class defines, and encapsulates an objects state and behavior. State tracks attributes, and is created at the object level
 # whereas behavior is what an object is capable of, and is shared at the class level.  instantiated objects of a class all share the behaviors defined by the class.  
 # Instance variables a.k.a. 'an objects state' are not shared, it's only available to the specific instantiated object.
 # Which is how different objects of the same class can hold different states from one another.
@@ -124,7 +130,7 @@ class Dog
 end
 
 doggie = Dog.new
-doggie.speak # speak is an instance method that the instantiated Dog object, held by the variable doggie, is invoking.
+doggie.speak # speak is an instance method that an instantiated Dog object, being pointed at by the variable doggie, is invoking.
 
 # -----------------------------------------------------------
 
@@ -145,4 +151,14 @@ end
 # if you only want to define the getter method use 'attr_reader'
 # if you only want to define the setter method use 'attr_writer'
 # if you want to define both the getter and setter method use 'attr_accessor'
+
+# -----------------------------------------------------------
+
+# OBJECTS
+
+# objects are instantiations of a class.  A class is what defines an object.  Objects have state and behavior. 
+# State, or an objects attributes are created at the object level and are unique to each individual instance. 
+# Behaviors are shared at the class level, by all instances of the same class.
+
+# ------------------------------------------------------------
 

502fe8a19b013a8b59e7b3dcdb25aceec01f8460
Jason Nelson <Jasonn318@gmail.com>
2018-11-06 21:56:49 -0600

    added classes LowerSection and UpperSection to act as colaborator obects within YahtzeeCard

--- yahtzee.rb
+++ yahtzee.rb
@@ -1,11 +1,32 @@
+# Select Number of players
+# Players set name
+# Players roll starting with player 1
+  # if the player still has unmarked values on their card, the player chooses which dice to keep (select dice number above the die)
+  # player rolls 2nd time with non kept dice. chooses which dice to keep (the kept dice from prev roll must be selected again)
+  # player rolls 3rd time, then chooses where to make mark (numbered rows) if the score is zero it will prompt you to confirm to put zero.
+  # changes to next player and continues the loop.
+# Grand Scores output.
+# Winner announced.
+# Play again?
+
 class YahtzeeCard
   def initialize
-    @upper_section = %w[ones twos threes fours fives sixes].each_with_object(Hash.new) do |number, upper_section|
-      upper_section[number] = 0
-    end
+    @upper_section = UpperSection.new
+    @lower_section = LowerSection.new
+    @grand_total = 0
   end
+end
 
+class LowerSection < YahtzeeCard
+  def initialize
 
+  end
+end
+
+class UpperSection < YahtzeeCard
+  def initialize
+
+  end
 end
 
 class Dice
@@ -14,4 +35,4 @@ class Dice
   end
 end
 
-p YahtzeeCard.new
+YahtzeeCard.new

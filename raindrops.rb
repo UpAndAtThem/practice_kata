2463b504d02cd0af8d2f56a6633658bde6f40046
Jason Nelson <Jasonn318@gmail.com>
2018-12-23 22:17:30 -0600

    refactored raindrops.rb

--- raindrops.rb
+++ raindrops.rb
@@ -1,12 +1,15 @@
 # Raindrops class
+
 class Raindrops
   NOISE = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
 
   def self.convert(int)
-    drop = [3, 5, 7].each.with_object('') do |poss_factor, drop_sound|
-      drop_sound << NOISE[poss_factor] if (int % poss_factor).zero?
-    end
+    drop = [3, 5, 7].map do |poss_factor|
+      (int % poss_factor).zero? ? NOISE[poss_factor] : ''
+    end.join
 
     drop.empty? ? int.to_s : drop
   end
 end
+
+

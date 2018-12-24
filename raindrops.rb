e0d9cde6ab17d1762628e485ea826cbee362144a
Jason Nelson <Jasonn318@gmail.com>
2018-12-23 20:24:49 -0600

    refactored raindrops.rb

--- raindrops.rb
+++ raindrops.rb
@@ -1,21 +1,12 @@
 # Raindrops class
 class Raindrops
-  def self.factor?(int, poss_factor)
-    (int % poss_factor).zero?
-  end
-
-  def self.convert_to_raindrop(int)
-    if factor?(int, 3) then 'Pling'
-    elsif factor?(int, 5) then 'Plang'
-    elsif factor?(int, 7) then 'Plong'
-    else int.to_s
-    end
-  end
+  NOISE = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
 
   def self.convert(int)
-    factors = [3, 5, 7].select { |test_factor| factor? int, test_factor }
-    rain_drop_sound = factors.map { |factor| convert_to_raindrop factor }.join
+    drop = [3, 5, 7].each.with_object('') do |poss_factor, drop_sound|
+      drop_sound << NOISE[poss_factor] if (int % poss_factor).zero?
+    end
 
-    factors.empty? ? int.to_s : rain_drop_sound
+    drop.empty? ? int.to_s : drop
   end
 end

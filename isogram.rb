2e7b85fd7158b4373c27721cbf8e6cb247e368ea
Jason Nelson <Jasonn318@gmail.com>
2018-12-28 21:18:18 -0600

    refactored from iterative to uniq

--- isogram.rb
+++ isogram.rb
@@ -1,11 +1,7 @@
 # Isogram class
 class Isogram
   def self.isogram?(test_string)
-    test_string.downcase.chars.each_with_object(Hash.new(0)) do |char, count_hsh|
-      count_hsh[char] += 1 if char =~ /[a-z]/
-      return false if count_hsh[char] > 1
-    end
-
-    true
+    word_chars_only = test_string.gsub(/\W/, '').downcase.chars
+    word_chars_only.uniq == word_chars_only
   end
 end

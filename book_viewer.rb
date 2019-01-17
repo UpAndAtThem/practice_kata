9b01195e32ea88b93eb57a5587f3be57ed5d37d3
Jason Nelson <Jasonn318@gmail.com>
2019-01-16 23:29:15 -0600

    added before

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,10 +1,24 @@
 require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
+require 'pry'
+
+before do
+  @contents = File.read('./data/toc.txt').split("\n")
+end
 
 get "/" do
-  b = binding
-  @table_of_contents = File.read('./data/toc.txt')
   @title = "The Adventures of Sherlock Holmes"
+  @chapter_num = "1"
   erb(:home)
+end
+
+get "/chapters/:number" do
+  @chapter_num = params[:number].to_i
+  @chapter_title = @contents[@chapter_num - 1]
+  @contents = File.read('./data/toc.txt').split("\n")
+  @title = "Chapter #{@chapter_num}: #{@chapter_title}"
+  @chapter_text = File.read("./data/chp#{@chapter_num}.txt").split("\n\n")
+  @chapter = "Chapter #{@chapter_num}"
+  erb(:chapter)
 end

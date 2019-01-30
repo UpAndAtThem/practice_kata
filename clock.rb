5dd59c293bab83a2d98bc8c38917910b79a1e7ea
Jason Nelson <Jasonn318@gmail.com>
2019-01-30 12:46:01 -0600

    added #- and #+ to Clock

--- clock.rb
+++ clock.rb
@@ -1,35 +1,22 @@
-# initialize_clock
-# analyze the clock hour and minute input.
-# analyze minutes: find the number of hours by doing integer division on it.  find the remainder of minutes and set that to minutes.
-# subtract or add extra hours from the minutes input.
-# analyze hours: find the remainder of hours and set that to minutes.
-
-
-require 'pry'
-
 class Clock
   def initialize(hour: (hour = 0), minute: (minute = 0))
     @hour = hour_adjusted(hour, minute)
     @minute = minute % MINUTES_IN_HOUR
-
-    @hour_str = "0" + @hour.to_s
-    @minute_str = "0" + @minute.to_s
-  end
-
-  def num_hours(minutes)
-    minutes / MINUTES_IN_HOUR
   end
 
   def to_s
-    "#{@hour_str[-2,2]}:#{@minute_str[-2,2]}"
+    "#{hour_str[-2,2]}:#{minute_str[-2,2]}"
   end
 
   def -(other)
+    neg_hour = other.hour * -1
+    neg_minute = other.minute * -1
 
+    other_clock_mutation(neg_hour, neg_minute, -1)
   end
 
   def +(other)
-
+    other_clock_mutation(other.hour, other.minute, 1)
   end
 
   def ==(other)
@@ -40,11 +27,34 @@ class Clock
 
   MINUTES_IN_HOUR = 60
   HOURS_IN_DAY = 24
-
-  attr_reader :hour, :minute, :hour_str, :minute_str
+  MINUTE_RANGE = (0..59)
 
   def hour_adjusted(hour, minute)
-    (num_hours(minute) + (hour % HOURS_IN_DAY)) % 24
+    (num_hours(minute) + hour) % HOURS_IN_DAY
   end
-end
 
+  def num_hours(minutes)
+    minutes / MINUTES_IN_HOUR
+  end
+
+  def hour_str
+    "0" + @hour.to_s
+  end
+
+  def minute_str
+    "0" + @minute.to_s
+  end
+
+  def other_clock_mutation(other_hour, other_minute, minute_rollover)
+    self.hour = self.hour + minute_rollover unless MINUTE_RANGE.cover?(self.minute + other_minute)
+
+    self.hour = (hour + other_hour) % 24
+    self.minute = (minute + other_minute) % 60
+
+    self
+  end
+
+  protected
+
+  attr_accessor :hour, :minute
+end

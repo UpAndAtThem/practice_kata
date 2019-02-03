e48b64a5798594514373bf64db0cb42c345b6049
Jason Nelson <Jasonn318@gmail.com>
2019-02-02 19:23:05 -0600

    add ffc.rb

--- ffc.rb
+++ ffc.rb
@@ -1,30 +1,18 @@
-def order_weight(string)
-  result_hsh = Hash.new {|h,k| h[k] = [] }
-  string_arr = string.split(" ")
-  count = 0
+class TestClass
+  private
+  @@count = 68
 
-  loop do
-    weight = string_arr[count].split("").map(&:to_i).reduce(&:+)
-    
-    result_hsh[weight] << string_arr[count]
-    count += 1
+  public
 
-    break if count == string_arr.size
+  def test
+    puts @@count
   end
 
-  result_hsh.values.each do |arr|
-    arr.sort_by! { |elem| elem}
+  def self.test_class_var
+    puts @@count
   end
-
-  result_hsh = result_hsh.to_a.sort_by{ |key, value| key}.to_h
-  
-  result_hsh.values.join(" ")
 end
 
-def order_weight_clever(string)
-  string.split.sort_by { |n| [n.chars.map(&:to_i).reduce(:+), n] }.join(" ") # the sort_by method will use n if there is a tie on the first condition
-end
+TestClass.new.test
 
-weight_list = "56 65 74 100 99 68 86 180 90"
-
-p order_weight weight_list
+TestClass.test_class_var

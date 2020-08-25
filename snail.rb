184c795ddfe3696631ea85b76848cb254058827b
Jason Nelson <Jasonn318@gmail.com>
2020-08-24 23:00:58 -0500

    add snail.rb

---
+++ snail.rb
@@ -0,0 +1,11 @@
+def snail(arrays)
+  width = arrays[0].length
+  height = arrays.size
+  p width, height
+  snail_arrs = []
+  arrays.each
+end
+
+arr = [[1, 2, 3],[8, 9, 4],[7, 6 ,5]]
+
+snail(arr)

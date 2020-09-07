660c26515c624f8a9e8785030e79ae60cb98449f
Jason Nelson <Jasonn318@gmail.com>
2020-09-07 12:07:43 -0500

    add rotate.js

---
+++ rotate.js
@@ -0,0 +1,33 @@
+function rotateArray(array) {
+  if(!Array.isArray(array)) {
+    return undefined;
+  }
+
+  if(array.length === 0) {
+    return [];
+  }
+
+  const arrayCopy = [...array];
+
+  let rotatingElement = arrayCopy.shift();
+  arrayCopy.push(rotatingElement);
+  console.log(arrayCopy);
+  return arrayCopy;
+}
+
+rotateArray([7, 3, 5, 2, 9, 1]);       // [3, 5, 2, 9, 1, 7]
+rotateArray(['a', 'b', 'c']);          // ["b", "c", "a"]
+rotateArray(['a']);                    // ["a"]
+rotateArray([1, 'a', 3, 'c']);         // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]);    // [[1, 2], 3, { a: 2 }]
+rotateArray([]);                       // []
+
+// return `undefined` if the argument is not an array
+rotateArray();                         // undefined
+rotateArray(1);                        // undefined
+
+
+// the input array is not mutated
+const array = [1, 2, 3, 4];
+rotateArray(array);                    // [2, 3, 4, 1]
+array;                                 // [1, 2, 3, 4]

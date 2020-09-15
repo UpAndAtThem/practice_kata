11da203044736022dfd8c1926b2039a8dcf8ce0d
Jason Nelson <Jasonn318@gmail.com>
2020-09-14 23:56:47 -0500

    add practice.sql

---
+++ practice.sql
@@ -0,0 +1,56 @@
+-- Create a table called countries. It should have the following columns:
+
+-- An id column of type serial
+-- A name column of type varchar(50)
+-- A capital column of type varchar(50)
+-- A population column of type integer
+-- The name column should have a UNIQUE constraint. The name and capital columns should both have NOT NULL constraints.
+
+CREATE TABLE countries(
+  id serial,
+  name varchar(50) UNIQUE NOT NULL,
+  capital varchar(50) NOT NULL,
+  population integer
+)
+
+-- Create a table called animals that could contain the sample data below:
+
+-- Name  Binomial Name Max Weight (kg) Max Age (years) Conservation Status
+-- Lion  Pantera leo 250 20  VU
+-- Killer Whale  Orcinus orca  6,000 60  DD
+-- Golden Eagle  Aquila chrysaetos 6.35  24  LC
+
+-- The database table should also contain an auto-incrementing id column.
+-- Each animal should always have a name and a binomial name.
+-- Names and binomial names vary in length but never exceed 100 characters.
+-- The max weight column should be able to hold data in the range 0.001kg to 40,000kg
+-- Conservation Status is denoted by a combination of two letters (CR, EN, VU, etc).
+
+
+CREATE TABLE animals(
+  id serial,
+  name varchar(100) NOT NULL,
+  binomial_name varchar(100) NOT NULL,
+  max_weight decimal CHECK (max_weight >= 0.001 AND max_weight <= 40000),
+  max_age integer,
+  conversion_status char(2)
+);
+
+-- Create a table in the ls_burger database called orders. The table should have the following columns:
+
+-- An id column, that should contain an auto-incrementing integer value.
+-- A customer_name column, that should contain a string of up to 100 characters
+-- A burger column, that should hold a string of up to 50 characters
+-- A side column, that should hold a string of up to 50 characters
+-- A drink column, that should hold a string of up to 50 characters
+-- An order_total column, that should hold a numeric value in dollars and cents. Assume that all orders will be less than $100.
+-- The customer_name and order_total columns should always contain a value.
+
+CREATE TABLE orders(
+  id serial,
+  customer_name varchar(100) NOT NULL,
+  burger varchar(50),
+  side varchar(50),
+  drink varchar(50),
+  order_total decimal(4, 2) NOT NULL
+);
